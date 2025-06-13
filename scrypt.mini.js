45b136b104ab130264f16057f39f929f30127bab
Matifernandezar <121912590+Matifernandezar@users.noreply.github.com>
2025-06-13 11:45:43 -0300

    Update scrypt.mini.js

--- scrypt.mini.js
+++ scrypt.mini.js
@@ -1,3 +1,4 @@
+
   document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("whatsapp-form").addEventListener("submit", e => {
       e.preventDefault();
@@ -6,7 +7,11 @@
         alert("Por favor, ingresa tu nombre antes de continuar.");
         return;
       }
-      const url = `https://wa.link/wm9668?text=${encodeURIComponent(`Hola, mi nombre es ${nombre}. Me gustaría tener más información para jugar.`)}`;
+
+      const mensaje = `Hola, mi nombre es ${nombre}. Me gustaría tener más información para jugar.`;
+      const numero = "5492975815752"; // <-- Cambiá este número si usás otro
+      const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
       window.open(url, "_blank");
     });
   });
+
