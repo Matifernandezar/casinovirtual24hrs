c96edee55183c7b01428f2191b143ca09b2a1b79
Matifernandezar <121912590+Matifernandezar@users.noreply.github.com>
2025-06-13 11:43:11 -0300

    Update scrypt.mini.js

--- scrypt.mini.js
+++ scrypt.mini.js
@@ -1,1 +1,12 @@
-document.addEventListener("DOMContentLoaded",()=>{document.getElementById("whatsapp-form").addEventListener("submit",e=>{e.preventDefault();let t=document.getElementById("name").value.trim();if(""===t)return void alert("Por favor, ingresa tu nombre antes de continuar.");window.open(`https://wa.link/wm9668?text=${encodeURIComponent(`Hola, mi nombre es ${t}. Me gustaría tener más información para jugar.`)}`,"_blank")})});
+  document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("whatsapp-form").addEventListener("submit", e => {
+      e.preventDefault();
+      const nombre = document.getElementById("name").value.trim();
+      if (!nombre) {
+        alert("Por favor, ingresa tu nombre antes de continuar.");
+        return;
+      }
+      const url = `https://wa.link/wm9668?text=${encodeURIComponent(`Hola, mi nombre es ${nombre}. Me gustaría tener más información para jugar.`)}`;
+      window.open(url, "_blank");
+    });
+  });
